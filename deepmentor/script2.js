c91c72ed6ccbecc5854ceca3abded3ba61b88cbd
sandy9999 <luckysandy99@gmail.com>
2018-05-27 09:55:37 +0530

    normalmodedone

--- deepmentor/script2.js
+++ deepmentor/script2.js
@@ -1,66 +1,61 @@
 var array = [];
-//var deleteee;
+
 function addfn()
 {
+	var todosUL = document.querySelector('ul');
+	if(array.length==0)
+	{
+		//console.log("hello");
+		todosUL.innerHTML = '';
+	}
+	//console.log(array);
 	var mentee = document.getElementById('mentee').value;
-	var todo = {name: mentee, rating:0/*, comments:inputcomments*/};
+	var todo = {id: array.length, name: mentee, rating:0, comments:''};
 	array.push(todo);
 	document.getElementById('mentee').value = '';
-	displayfn();
-	//console.log(array);
-	
-}
-
-function displayfn()
-{
-	var todosUl = document.querySelector('ul');
-    todosUl.innerHTML = '';
-	for(var i = 0; i<array.length; i++)
-	{
-		
 		var todoli = document.createElement('li');
+		todoli.id = "todoli" + array.length - 1;
 		var menteename = document.createElement('input');
 		menteename.setAttribute('type','text');
-		menteename.value = array[i].name;
-		menteename.id = "menteename";
+		menteename.value = array[array.length-1].name;
+		menteename.className = "menteename";
+		menteename.id = "menteename" + array.length-1;
 		var ratingbox = document.createElement("input");
 		ratingbox.setAttribute('type', 'number');
 		ratingbox.className = "ratingbox";
-		ratingbox.id = "ratingbox" + i;
-		if(array[i].rating != 0)
-			ratingbox.value = array[i].rating;
+		ratingbox.id = "ratingbox" + array.length-1;
 		var remarkarea = document.createElement('textarea');
+		remarkarea.className = "remarkarea";
+		remarkarea.id = "remarkarea" + array.length - 1;
 		var deletebutton = document.createElement('button');
 		deletebutton.innerHTML = 'Delete';
 		deletebutton.className = 'deletebutton';
-		deletebutton.id = i;
-		/*deletebutton.onclick = function()
+		deletebutton.id = "deletebutton" + array.length - 1;
+		todoli.appendChild(menteename);
+		todoli.appendChild(ratingbox);
+		todoli.appendChild(remarkarea);
+		todoli.appendChild(deletebutton);
+		todosUL.appendChild(todoli);
+		deletebutton.onclick = function()
 		{
-			array.splice(i,1);
-			console.log(array);
-			displayfn();
-		};*/
-		todosUl.appendChild(todoli);
-		todosUl.appendChild(menteename);
-		todosUl.appendChild(ratingbox);
-		todosUl.appendChild(remarkarea);
-		todosUl.appendChild(deletebutton);
-		
-	}
-	for(var i=0; i<array.length; i++)
-		{
-			if(document.getElementById("ratingbox" + i).value!='')
-				array[i].rating = document.getElementById("ratingbox" + i).value;
-		}
-		console.log(array);
-
+			array.splice(array.length - 1,1);
+			/*var ul = document.getElementsByTagName('ul')[0];
+			var li = document.getElementById('todoli' + array.length-1);
+			ul.removeChild(li);*/
+			todosUL.removeChild(this.parentNode);
+		};
+			//console.log(array);
+	
 }
+
 
 function sortfn()
 {
 	for(var i=0; i<array.length; i++)
 	{
-		array[i].rating = document.getElementById("ratingbox" + i).value;
+		if(document.getElementById("ratingbox" + array[i].id).value!='')
+		array[i].rating = document.getElementById("ratingbox" + array[i].id).value;
+		array[i].comments = document.getElementById("remarkarea"+array[i].id).value;
 	}
 	//console.log(array);
 	for(var i=0; i<(array.length -1) ; i++)
@@ -68,23 +63,58 @@ function sortfn()
 		{
 			if(array[i].rating > array[j].rating)
 			{
+				var temp = array[i].id;
+				array[i].id = array[j].id;
+				array[j].id = temp;
 				var temp = array[i].rating;
 				array[i].rating = array[j].rating;
 				array[j].rating = temp;
 				var temp = array[i].name;
 				array[i].name = array[j].name;
 				array[j].name = temp;
+				var temp = array[i].comments;
+				array[i].comments = array[j].comments;
+				array[j].comments = array[i].comments;
 			}
 		}
-	displayfn();
+		
+	var todosUL = document.querySelector('ul');
+	todosUL.innerHTML = '';
+	for(var i=0; i<array.length; i++)
+	{
+		var todoli = document.createElement('li');
+		todoli.id = "todoli" + i;
+		var menteename = document.createElement('input');
+		menteename.setAttribute('type','text');
+		menteename.value = array[i].name;
+		menteename.className = "menteename";
+		menteename.id = "menteename" + i;
+		var ratingbox = document.createElement("input");
+		ratingbox.setAttribute('type', 'number');
+		ratingbox.className = "ratingbox";
+		ratingbox.id = "ratingbox" + i;
+		var remarkarea = document.createElement('textarea');
+		remarkarea.className = "remarkarea";
+		remarkarea.id = "remarkarea" + i;
+		var deletebutton = document.createElement('button');
+		deletebutton.innerHTML = 'Delete';
+		deletebutton.className = 'deletebutton';
+		deletebutton.id = "deletebutton" + i;
+		todoli.appendChild(menteename);
+		todoli.appendChild(ratingbox);
+		todoli.appendChild(remarkarea);
+		todoli.appendChild(deletebutton);
+		todosUL.appendChild(todoli);
+		deletebutton.onclick = function()
+		{
+			array.splice(i,1);
+			/*var ul = document.getElementsByTagName('ul')[0];
+			var li = document.getElementById('todoli' + array.length-1);
+			ul.removeChild(li);*/
+			todosUL.removeChild(this.parentNode);
+		};
+	}
+	
 }
 
-/*deleteee = document.getElementsByClassName("deletebutton");
-for(var i = 0; i<deleteee.length; i++)
-{
-	deleteee[i].addEventListener('click',function()
-	{
-		array.splice(deleteee[i].id,1);
-	});
-	//console.log(deleteee[i]);
-}*/
+
